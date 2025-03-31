c3ecc79f47320e5563f01ca5a4c29f309a2a92d6
JackOehling <oehling.j@northeastern.edu>
2025-03-30 20:35:33 -0400

    Initial Commit

---
+++ roomie/roomieApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomieApp.swift
+//  roomie
+//
+//  Created by Jack Oehling on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct roomieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ roomieTests/roomieTests.swift
@@ -0,0 +1,17 @@
+//
+//  roomieTests.swift
+//  roomieTests
+//
+//  Created by Jack Oehling on 3/30/25.
+//
+
+import Testing
+@testable import roomie
+
+struct roomieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
